b15075bbe2ff4df80415c81e877ce6aad6555c35
yjcmmy <18717878625@163.com>
2019-07-12 21:45:38 +0800

    7.12

---
+++ JVM/src/cglib/MyCglib.java
@@ -0,0 +1,43 @@
+package cglib;
+
+import net.sf.cglib.core.DebuggingClassWriter;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class MyCglib {
+    public static void main(String[] args) {
+        // 代理类class文件存入本地磁盘方便我们反编译查看源码
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "C:\\Code\\learn\\JVM\\com\\sun");
+        // 通过CGLIB动态代理获取代理对象的过程
+        Enhancer enhancer = new Enhancer();
+        // 设置enhancer对象的父类
+        enhancer.setSuperclass(HelloService.class);
+        // 设置enhancer的回调对象
+        enhancer.setCallback(new MyMethodInterceptor());
+        // 创建代理对象
+        HelloService proxy= (HelloService)enhancer.create();
+        // 通过代理对象调用目标方法
+        proxy.sayHello();
+    }
+
+
+}
+
+class MyMethodInterceptor  implements MethodInterceptor{
+    /**
+     * sub：cglib生成的代理对象
+     * method：被代理对象方法
+     * objects：方法入参
+     * methodProxy: 代理方法
+     */
+    @Override
+    public Object intercept(Object sub, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("======插入前置通知======");
+        Object object = methodProxy.invokeSuper(sub, objects);
+        System.out.println("======插入后者通知======");
+        return object;
+    }
+}
